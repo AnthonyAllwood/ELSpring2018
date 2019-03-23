bfd03cc7358af2ea276a0632a471fe139e505d12
Anthony Allwood <a.allwood9@gmail.com>
2019-03-23 02:32:03 -0400

    Modified to test if sensors are functional!!

--- midterm/midterm.py
+++ midterm/midterm.py
@@ -1,35 +1,31 @@
 #!/usr/bin/python
 
-#Import Libraries we will be using
+#Import these libraries
 import RPi.GPIO as GPIO
 import time
 import os
 import sqlite3
 import sys
 
-#Assign GPIO pins
+#Assign GPIO pins 13 and 26
 entrySensor = 13
 exitSensor = 26
 
-#Stabilization time
-stableTime = 5
-#---------------------------------------------------------------------
-#Initialize the GPIO
+#Initialize GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(entrySensor, GPIO.IN)
 GPIO.setup(exitSensor, GPIO.IN)
 
+#------------------------------------------------
 #SQLite setup
 db = sqlite3.connect('./log/logPeople.db')
 cursor = db.cursor() #Get cursor
 db.commit() #Commit Changes
-
-#Using Simon's code
+#------------------------------------------------
 
 def bothTriggers(trigger2, wait=5):
 	timeStamp = False
-	#This is the sanity check.  If the second sensor isn't triggered, it resets.
-	#The sanity check only happens while the second sensor is in a low state (not triggered)
+	#In case second sensor is not triggered
 	timeCheck = time.time()
 	while not GPIO.input(trigger2):
 		if time.time() - timeCheck > wait:
@@ -40,30 +36,36 @@ def bothTriggers(trigger2, wait=5):
         		time.sleep(4)
         	continue
 
-#Stabilize sensor
+#Sensor stabilization for 10 seconds
 time.sleep(10)
-#Initialize people in room
+
+#Keeps track of how many people are in the room
 peopleCount = 0
 
 try:
 	while True:
-		#Reset timeStamp to false to prevent writing data until both sensors are triggered again
+		#This resets the timeStamp to false to prevent writing data until both sensors are triggered again
 		timeStamp = False
-		#set entry Type
+
+		#This sets the entry Type
 		inOrOut = "IDLE"
 
         	if GPIO.input(entrySensor):
+			#This print line tests to see if entrySensor is functional
+			print("entrySensor triggered")
 			timeStamp = bothTriggers(exitSensor)
 			if timeStamp:
 				inOrOut = "Entrance"
 				peopleCount = peopleCount + 1
 		if GPIO.input(exitSensor):
+			#This print line tests to see if exitSensor is functional
+			print("exitSensor triggered")
 			timeStamp = bothTriggers(entrySensor)
 			if timeStamp:
 				inOrOut = "Exit"
 				peopleCount = peopleCount - 1
 
-#Since the timeStamp is only set when the direction is determined and both sensors are triggered we use that as the condition to write our data:
+#Writes data to the logPeople table in the database:
 		if timeStamp:
 			cursor.execute('''INSERT INTO logPeople VALUES(?,?,?)''', (timeStamp, inOrOut, peopleCount))
 			db.commit()
@@ -71,6 +73,8 @@ try:
 			os.system('clear')
 			for row in all_rows:
 				print('{0} : {1} : {2}'.format(str(row[0]), row[1], str(row[2])))
+
+	time.sleep(10)
 
 except db.Error, e:
 	print "Error %s:" %e.args[0]
