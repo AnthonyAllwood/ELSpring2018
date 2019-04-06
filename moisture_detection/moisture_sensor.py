43a13a79d6f6606db4b5cbe8513a194e5adc3cd6
Anthony Allwood <a.allwood9@gmail.com>
2019-04-05 20:20:56 -0400

    Updated to send emails on moisture detection

--- moisture_detection/moisture_sensor.py
+++ moisture_detection/moisture_sensor.py
@@ -2,7 +2,7 @@
 
 #Import necessary libraries
 
-import Rpi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 import os
 import sqlite3  #This library will be used to log the timestamp and moisture readings to the database
@@ -11,43 +11,48 @@ import smtplib  #This is the library that will be utilized in order to send emai
 #Assign GPIO pin
 
 sensorPin = 27
-analogPin = 26
+
 #Initialize the GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(sensorPin, GPIO.IN)
-GPIO
+
 #Variable definitions
 
-detectNum = 3
+detectNum = 1
 
-user_smtp = "a.allwood9@gmail.com" #SMTO provider username
-password_smtp = "TB2freshh23" #SMTP provider password
-host_smtp = "allwooda1" #SMTP provider hostname
-port_smtp = 25 #SMTP provider port
-email_receiver = ['a.allwood9@gmail.com']
+smtpUser = 'a.allwood9@gmail.com' #SMTO provider username
+smtpPass = 'TB2freshh23' #SMTP provider password
+emailSender = 'allwooda1@allwoodpi'
+emailReceiver = 'a.allwood9@gmail.com'
 
 #Message strings
 
-no_moisture = ''' From: Anthony Pi <allwoodpi>
+no_moisture = """ From: Anthony Pi <allwoodpi>
 To: Anthony Allwood <a.allwood9@gmail.com>
 Subject: Moisure Sensor Message !!!
 
-NO MOISTURE DETECTED ! '''
+NO MOISTURE DETECTED ! """
 
-moisture_detected = ''' From: Anthony Pi <allwoodpi>
+moisture_detected = """ From: Anthony Pi <allwoodpi>
 To: Anthony Allwood <a.allwood9@gmail.com>
 Subject: Moisture Sensor message !!!
 
-MOISTURE DETECTED ! '''
+MOISTURE DETECTED ! """
 
 #Function to send email
 
 def sendEmail(message_send):
 	try:
-		object_Smtp = stmplib.SMTP(host_smtp, port_smtp)
-		object_Smtp.login(user_smtp, password_smtp)
-		object_Smtp.sendmail(email_receiver, message_send)
+		smtpObject = smtplib.SMTP('smtp.gmail.com', 587)
+		smtpObject.ehlo()
+		smtpObject.starttls()
+		smtpObject.ehlo()
+
+		smtpObject.login(smtpUser, smtpPass)
+		smtpObject.sendmail(emailSender, emailReceiver, message_send)
+
+		smtpObject.quit()
 
 		print "Email Successfully Sent!"
 
@@ -63,7 +68,22 @@ def isSensorOn(sensorPin):
 
 	else:
 		print "MOISTURE DETECTION ON"
-		sendEmail(moisture_detection)
+		sendEmail(moisture_detected)
 
 
-def readSensorH(analogPin)
+try:
+
+	while True:
+
+			for i in range (detectNum):
+				isSensorOn(sensorPin)
+
+			time.sleep(30)
+
+			#END
+
+except KeyboardInterrupt:
+	os.system('clear')
+	print('Moisture Detection Complete!')
+	GPIO.cleanup()
+
